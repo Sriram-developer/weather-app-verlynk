a1a6e511f7f9f60758a5c938de234f69c055a772
Sriram <sri761177@gmail.com>
2024-04-08 19:11:25 +0530

    Add react form and select for city management

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Select from "react-select";
 
 function App() {
   return (
     <div>
-      <h1>Weather App</h1>
+      {/* Sidebar */}
+      <div className="flex flex-col space-y-3">
+       {/* Form */}
+       <Select/>
+
+       <Select/>
+       <button className="bg-green-400 w-full py-3 text-white text-sm font-bold hover:scale-105 transition-all duration-200 ease-in-out">Get Weather</button>
+      </div>
+      {/* Body */}
+      <div></div>
     </div>
   );
 }
