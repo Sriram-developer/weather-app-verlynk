47e90d41fa582f99e846ccfe5da374b58db03afd
Sriram <sri761177@gmail.com>
2024-04-09 20:10:42 +0530

    add termor and fetched the open api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import Select from "react-select";
 import { City, Country } from "country-state-city";
+import { Card,Metric,Title } from "@tremor/react"
 
 function App() {
   const [allCountries, setAllCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState([]);
   const [selectedCity, setSeletedCity] = useState([]);
+  const [weatherDetails, setWeatherDetails] = useState([]);
 
   useEffect(() => {
     setAllCountries(
@@ -29,11 +31,18 @@ function App() {
     setSeletedCity(option);
    };
 
+   const getWeatherDetails  = async (e) => {
+       e.preventDefault();
+
+       // fetching the open api  
+       const fetchWeather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${selectedCity?.values?.latitude}&longitude=${selectedCity?.values?.longitude}&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,wind_speed_180m,temperature_180m,is_day&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,uv_index_clear_sky_max,wind_speed_10m_max`)
+   }
+
   
   return (
-    <div>
+    <div className="flex max-7xl mx-auto space-x-2  py-10">
       {/* Sidebar */}
-      <div className="flex flex-col space-y-3">
+      <div className="flex flex-col space-y-3 h-screen bg-blue-950 p-3 w-[20%]">
        {/* Form */}
        <Select options={allCountries} value={selectedCountry} onChange={handleSelectedCountry}/>
 
@@ -50,14 +59,37 @@ function App() {
        onChange={handleSelectedCity}
        />
 
-       <button className="bg-green-400 w-full py-3 text-white text-sm font-bold hover:scale-105 transition-all duration-200 ease-in-out">Get Weather</button>
-       <div>
+       <button onClick={getWeatherDetails} className="bg-green-400 w-full py-3 text-white text-sm font-bold hover:scale-105 transition-all duration-200 ease-in-out">Get Weather</button>
+      
+       <div className="flex flex-col space-y-2 text-white font-semibold">
         <p>{selectedCountry?.label} | {selectedCity?.label}</p> 
         <p>Cooridinates: {selectedCity?.value?.latitude} | {" "} {selectedCity?.value?.longitude}</p>
        </div>
+       
+       <div>
+        {/* Sunrise or Sunset */}
+       </div>
+       
       </div>
       {/* Body */}
-      <div></div>
+      <div className="w-[75%] h-screen">
+        <div className="flex item-center space-x-2">
+          <Card decoration="top" decorationColor="green" className="bg-gray-100 text-center">
+            <Title>Temperature</Title>
+            <Metric>23</Metric>
+          </Card>
+
+          <Card decoration="top" decorationColor="green" className="bg-gray-100 text-center">
+            <Title>Temperature</Title>
+            <Metric>23</Metric>
+          </Card>
+
+          <Card decoration="top" decorationColor="green" className="bg-gray-100 text-center">
+            <Title>Temperature</Title>
+            <Metric>23</Metric>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
